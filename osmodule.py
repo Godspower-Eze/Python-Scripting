ed83fda6c6e629f310a643d1d0b83697c11390f8
Godspower Eze <Godspowereze260@gmail.com>
2020-05-25 05:14:14 +0100

    added more funtionalities to the os module

--- osmodule.py
+++ osmodule.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 
 print(os.getcwd())
 
@@ -15,7 +16,21 @@ print(os.path.join('/home/eminentfablous/PycharmProjects/PythonScripting',
 print(os.path.split('/home/eminentfablous/PycharmProjects/PythonScripting/sysmodule.py'))
 
 print(os.path.exists('/home/eminentfablous/PycharmProjects/PythonScripting/sysmodule.py'))
-
+"""
 pas = dir(os)
 for i in pas:
     print(i)
+
+"""
+
+# os.mkdir('OSDD')
+# os.makedirs('opp/hhh/iiii')
+
+# os.rmdir('OSDD')
+# os.removedirs('opp/hhh/iiii')
+
+# os.rename('Videos', 'vid')
+print(os.listdir())
+print(os.stat('vid').st_mtime)
+mod_time = os.stat('vid').st_mtime
+print(datetime.fromtimestamp(mod_time))
